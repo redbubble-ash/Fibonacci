b0401451db49a4eb0b3ffff818d4ac80397c1d66
Jing Xie <xiejing418@gmail.com>
2019-10-10 09:10:16 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("The fibonacci result for index of 8 is " + Recursion(6));
+            
+        }
+
+        static int Recursion(int i)
+        {
+            if(i >0 )
+            {
+                if (i == 0 || i == 1)
+                {
+                    return i;
+                }
+                else
+                {
+                    return Recursion(i - 1) + Recursion(i - 2);
+                }
+            }
+            else
+            {
+                return  0;
+            }
+        }
+    }
+}
